ce875d75fe1fa95dd585a09ae3ee2f0f554f340a
Kalunnenna1 <nnennakalualie@yahoo.com>
2023-01-30 08:51:09 +0000

    4-user-promise.js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,6 +1,4 @@
-function signUpUser(firstName, lastName) {
+export default function signUpUser(firstName, lastName) {
   return Promise.resolve({ firstName, lastName });
 }
-
-export default signUpUser;
 
