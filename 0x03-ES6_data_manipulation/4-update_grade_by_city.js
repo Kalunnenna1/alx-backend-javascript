60dc5e014b0cd78d8cbcf28494e18cdd9609a4b9
Kalunnenna1 <nnennakalualie@yahoo.com>
2023-02-01 16:15:15 +0000

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,25 +1,25 @@
-// Grade by city with new grade
+/*
+ * File: 4-update_grade_by_city.js
+ * Auth: Nne
+ */
 
-const updateStudentGradeByCity = (students, city, newGrades) => {
-  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
-    return [];
-  }
-
-  const studentByCity = students
-    .filter((student) => student.location === city)
+/**
+ * updateStudentGradeByCity - Retrieves a list of students by city with their grades.
+ * @studentList: A list of students.
+ * @city: Student's location.
+ * @newGrades:Student's grade.
+ * Return: An array of students for a specific city with their new grade.
+ */
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  return studentList
+    .filter((item) => item.location === city)
     .map((student) => {
-      const grades = newGrades.filter((note) => student.id === note.studentId);
-      let grade = 'N/A';
-
-      if (grades[0]) {
-        grade = grades[0].grade;
-      }
-
-      return { ...student, grade };
+      const grades = newGrades.filter((item) => item.studentId === student.id);
+      const grade = grades.length ? grades[0].grade : 'N/A';
+      return {
+        ...student,
+        grade,
+      };
     });
-
-  return studentByCity;
-};
-
-export default updateStudentGradeByCity;
+}
 
