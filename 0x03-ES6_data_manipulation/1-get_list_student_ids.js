95a4fa9972b89b4c18ea23ac59022d8f3eaa3a0f
Kalunnenna1 <nnennakalualie@yahoo.com>
2023-02-01 16:09:38 +0000

    1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,17 +1,12 @@
-/*
- * File: 1-get_list_student_ids.js
- * Auth: Nne
- */
+// List students
 
-/**
- * getListStudentIds - Retrieves the id of student's list.
- * @studentList: List of students
- * Return: An array of ids from a list of object.
- */
-export default function getListStudentIds(studentList) {
-  if (studentList instanceof Array) {
-    return studentList.map((item) => item.id);
+const getListStudentIds = (ids) => {
+  if (!Array.isArray(ids)) {
+    return [];
   }
-  return [];
-}
+  const meids = ids.map((item) => item.id);
 
+  return meids;
+};
+
+export default getListStudentIds;
