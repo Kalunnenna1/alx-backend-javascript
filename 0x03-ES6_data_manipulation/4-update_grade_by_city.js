d96c63f7dfd9ee8d0a81dd37fa110c7f356907fb
Kalunnenna1 <nnennakalualie@yahoo.com>
2023-02-01 16:12:24 +0000

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,18 +1,25 @@
-function updateStudentGradeByCity(students, city, newGrades) {
-  const studentsCity = students.filter((student) => student.location === city);
-  return studentsCity.map((student) => {
-    const filteredGrades = newGrades.filter((grade) => grade.studentId === student.id);
-    if (filteredGrades.length > 0) {
-      return {
-        ...student,
-        grade: filteredGrades[0].grade,
-      };
-    }
-    return {
-      ...student,
-      grade: 'N/A',
-    };
-  });
-}
+// Grade by city with new grade
+
+const updateStudentGradeByCity = (students, city, newGrades) => {
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
+
+  const studentByCity = students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const grades = newGrades.filter((note) => student.id === note.studentId);
+      let grade = 'N/A';
+
+      if (grades[0]) {
+        grade = grades[0].grade;
+      }
+
+      return { ...student, grade };
+    });
+
+  return studentByCity;
+};
 
 export default updateStudentGradeByCity;
+
